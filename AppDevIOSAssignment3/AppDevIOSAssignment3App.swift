3dd60ef23248d1a40dbb6ba3fd902547edde17be
a-phu <annabel.phu@hotmail.com>
2023-05-10 08:54:30 +1000

    Initial Commit

---
+++ AppDevIOSAssignment3/AppDevIOSAssignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  AppDevIOSAssignment3App.swift
+//  AppDevIOSAssignment3
+//
+//  Created by Annabel Phu on 10/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppDevIOSAssignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
